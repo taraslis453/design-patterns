3c53c883d511fb0c00b3303c633f153715d570cb
taraslis453 <taraslisiy54@gmail.com>
2022-06-18 12:16:32 +0300

    feat(patterns): add factory method

---
+++ patterns/creational/factory-method/factory-method.go
@@ -0,0 +1,53 @@
+package factorymethod
+
+import "fmt"
+
+type Speaker interface {
+	Speak() string
+}
+
+type Dog struct {
+	name string
+}
+
+func NewDog(name string) Dog {
+	return Dog{
+		name: name,
+	}
+}
+
+func (d Dog) Speak() string {
+	return fmt.Sprint("woof")
+}
+
+type Cat struct {
+	name string
+}
+
+func NewCat(name string) Cat {
+	return Cat{
+		name: name,
+	}
+}
+
+func (c Cat) Speak() string {
+	return fmt.Sprint("Meow")
+}
+
+// factory method
+func SpeakerFactory(t, name string) Speaker {
+	switch {
+	case t == "dog":
+		return NewDog(name)
+	case t == "cat":
+		return NewCat(name)
+	}
+	return nil
+}
+
+func FactoryMethod() {
+	d := SpeakerFactory("dog", "marshal")
+	fmt.Println(d.Speak())
+	c := SpeakerFactory("cat", "pi")
+	fmt.Println(c.Speak())
+}
