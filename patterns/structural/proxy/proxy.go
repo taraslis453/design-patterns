17f943d5e987a7077896d41e7598c69a357ccbcc
taraslis453 <taraslisiy54@gmail.com>
2022-07-13 17:23:02 +0300

    feat(patterns/structural): add proxy

---
+++ patterns/structural/proxy/proxy.go
@@ -0,0 +1,34 @@
+package proxy
+
+import "fmt"
+
+type service interface {
+	DoSomething()
+}
+
+type realService struct {
+	ServiceName string
+}
+
+func (s realService) DoSomething() {
+	// do something
+	fmt.Println("Doing something")
+}
+
+func newServiceProxy(serviceName string) service {
+	return realServiceProxy{realService{serviceName}}
+}
+
+type realServiceProxy struct {
+	realService
+}
+
+func (p realServiceProxy) DoSomething() {
+	fmt.Println("Doing something with proxy")
+	p.realService.DoSomething()
+}
+
+func RunProxy() {
+	service := newServiceProxy("service")
+	service.DoSomething()
+}
--- patterns/structural/structural.go
+++ patterns/structural/structural.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 
 	"github.com/taraslis453/design-patterns/patterns/structural/adapter"
+	"github.com/taraslis453/design-patterns/patterns/structural/proxy"
 )
 
 func Structural() {
 	fmt.Println("Structural ----------")
 	fmt.Println("Adapter")
 	adapter.Adapter()
+	fmt.Println("Proxy")
+	proxy.RunProxy()
 }
